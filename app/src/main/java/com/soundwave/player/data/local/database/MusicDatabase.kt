d8b4555a375c983ea452a36f4dc12c69a6a57cea
Emadhamy <emadh1010@gmail.com>
2026-02-03 00:55:34 +0200

    Create app/src/main/java/com/soundwave/player/data/local/database/MusicDatabase.kt

---
+++ app/src/main/java/com/soundwave/player/data/local/database/MusicDatabase.kt
@@ -0,0 +1,39 @@
+package com.soundwave.player.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.soundwave.player.data.local.database.dao.*
+import com.soundwave.player.data.local.database.entities.*
+
+@Database(
+    entities = [
+        SongEntity::class,
+        PlaylistEntity::class,
+        PlaylistSongCrossRef::class,
+        QueueEntity::class,
+        PlaybackStateEntity::class,
+        EqualizerSettingsEntity::class,
+        AppSettingsEntity::class,
+        SleepTimerSettingsEntity::class,
+        LyricsEntity::class
+    ],
+    version = 1,
+    exportSchema = true
+)
+@TypeConverters(Converters::class)
+abstract class MusicDatabase : RoomDatabase() {
+    
+    abstract fun songDao(): SongDao
+    abstract fun playlistDao(): PlaylistDao
+    abstract fun settingsDao(): SettingsDao
+    abstract fun lyricsDao(): LyricsDao
+    
+    companion object {
+        const val DATABASE_NAME = "soundwave_database"
+    }
+}
+
+class Converters {
+    // يمكن إضافة محولات هنا إذا لزم الأمر
+}
