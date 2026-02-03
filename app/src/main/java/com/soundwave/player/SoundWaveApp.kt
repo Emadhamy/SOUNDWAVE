93667b0d9e11272449391cddc6dd8eb882583c3c
Emadhamy <emadh1010@gmail.com>
2026-02-03 13:02:11 +0200

    Create app/src/main/java/com/soundwave/player/SoundWaveApp.kt

---
+++ app/src/main/java/com/soundwave/player/SoundWaveApp.kt
@@ -0,0 +1,37 @@
+package com.soundwave.player
+
+import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SoundWaveApp : Application(), ImageLoaderFactory {
+    
+    override fun onCreate() {
+        super.onCreate()
+    }
+    
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(cacheDir.resolve("image_cache"))
+                    .maxSizePercent(0.05)
+                    .build()
+            }
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .crossfade(true)
+            .crossfade(200)
+            .build()
+    }
+}
