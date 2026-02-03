2ce67eaed196780146d870b6de676824caf77114
Emadhamy <emadh1010@gmail.com>
2026-02-03 12:56:03 +0200

    Create app/src/main/java/com/soundwave/player/ui/theme/Color.kt

---
+++ app/src/main/java/com/soundwave/player/ui/theme/Color.kt
@@ -0,0 +1,64 @@
+package com.soundwave.player.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary Colors
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF8B5CF6)
+val PurpleGrey40 = Color(0xFF625B71)
+val Pink40 = Color(0xFF7D5260)
+
+// Dark Theme Colors
+val DarkBackground = Color(0xFF0D0D0D)
+val DarkSurface = Color(0xFF1A1A1A)
+val DarkSurfaceVariant = Color(0xFF2D2D2D)
+val DarkSurfaceContainer = Color(0xFF242424)
+
+// Light Theme Colors
+val LightBackground = Color(0xFFFAFAFA)
+val LightSurface = Color(0xFFFFFFFF)
+val LightSurfaceVariant = Color(0xFFF0F0F0)
+val LightSurfaceContainer = Color(0xFFE8E8E8)
+
+// Accent Colors
+val AccentPurple = Color(0xFF8B5CF6)
+val AccentCyan = Color(0xFF06B6D4)
+val AccentPink = Color(0xFFEC4899)
+val AccentGreen = Color(0xFF10B981)
+val AccentOrange = Color(0xFFF97316)
+val AccentRed = Color(0xFFEF4444)
+val AccentBlue = Color(0xFF3B82F6)
+val AccentYellow = Color(0xFFFACC15)
+
+// Gradient Colors
+val GradientPurpleCyan = listOf(Color(0xFF8B5CF6), Color(0xFF06B6D4))
+val GradientPinkOrange = listOf(Color(0xFFEC4899), Color(0xFFF97316))
+val GradientGreenCyan = listOf(Color(0xFF10B981), Color(0xFF06B6D4))
+
+// Text Colors - Dark Theme
+val TextPrimaryDark = Color(0xFFF8FAFC)
+val TextSecondaryDark = Color(0xFF94A3B8)
+val TextTertiaryDark = Color(0xFF64748B)
+
+// Text Colors - Light Theme
+val TextPrimaryLight = Color(0xFF1E293B)
+val TextSecondaryLight = Color(0xFF475569)
+val TextTertiaryLight = Color(0xFF94A3B8)
+
+// Status Colors
+val Success = Color(0xFF10B981)
+val Warning = Color(0xFFFACC15)
+val Error = Color(0xFFEF4444)
+val Info = Color(0xFF3B82F6)
+
+// Visualizer Colors
+val VisualizerColors = listOf(
+    Color(0xFF8B5CF6),
+    Color(0xFF06B6D4),
+    Color(0xFFEC4899),
+    Color(0xFF10B981),
+    Color(0xFFF97316)
+)
